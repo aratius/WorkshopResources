e18c814d1c899ffd7cc8085d62e340ea6e7ce35e
aratius <arata1129matsu@icloud.com>
2023-03-08 16:38:44 +0900

    player color

--- StreetFighter/Assets/Scripts/Fighter/Enemy.cs
+++ StreetFighter/Assets/Scripts/Fighter/Enemy.cs
@@ -6,73 +6,81 @@ using DG.Tweening;
 public class Enemy : Fighter
 {
 
-    List<Fighter> m_Targets = new List<Fighter>();
-    float m_LastAttackedTime = 0;
-    float m_TargetDirection = 0;
-    Tween m_Tween;
+  List<Fighter> m_Targets = new List<Fighter>();
+  float m_LastAttackedTime = 0;
+  float m_TargetDirection = 0;
+  Tween m_Tween;
 
-    void Start()
-    {
-        base.Start();
-    }
+  void Start()
+  {
+    base.Start();
+  }
 
-    void Update()
+  void Update()
+  {
+    base.Update();
+    if (m_IsFreezing) return;
+    if (m_Targets.Count > 0)
     {
-        base.Update();
-        if(m_IsFreezing) return;
-        if(m_Targets.Count > 0)
+      float nearest = 999;
+      Fighter nearestTarget = m_Targets[0];
+      foreach (Fighter target in m_Targets)
+      {
+        float dist = Vector3.Distance(transform.position, target.transform.position);
+        if (dist < nearest)
         {
-            float nearest = 999;
-            Fighter nearestTarget = m_Targets[0];
-            foreach (Fighter target in m_Targets)
-            {
-                float dist = Vector3.Distance(transform.position, target.transform.position);
-                if(dist < nearest)
-                {
-                    nearest = dist;
-                    nearestTarget = target;
-                }
-            }
-
-            Vector3 direction = nearestTarget.transform.position - transform.position;
-            if(direction.magnitude < 1.5f)
-            {
-                InvokeAttack();
-            }
-
-            if(direction.magnitude < 2f)
-            {
-                Walk(Mathf.Sign(m_TargetDirection));
-            }
-            else if(direction.magnitude < 6f)
-            {
-                Run(Mathf.Sign(m_TargetDirection));
-            }
-            // TODO: なんもない時うろうろしたい
-
-            if(m_Tween != null) m_Tween.Kill();
-            m_Tween = DOTween.To(
-                () => m_TargetDirection,
-                v => m_TargetDirection = v,
-                direction.x,
-                1f
-            );
+          nearest = dist;
+          nearestTarget = target;
         }
-    }
+      }
 
-    public void AddTarget(Fighter target)
-    {
-        m_Targets.Add(target);
-    }
+      Vector3 direction = nearestTarget.transform.position - transform.position;
+      if (direction.magnitude < 2f)
+      {
+        InvokeAttack();
+      }
 
-    void InvokeAttack()
-    {
-        float interval = 1f;
-        if(UnityEngine.Time.time - m_LastAttackedTime > interval)
-        {
-            Attack();
-            m_LastAttackedTime = UnityEngine.Time.time;
-        }
+      if (direction.magnitude < 2f)
+      {
+        Walk(Mathf.Sign(m_TargetDirection));
+      }
+      else if (direction.magnitude < 6f)
+      {
+        Run(Mathf.Sign(m_TargetDirection));
+      }
+      else
+      {
+        float t = UnityEngine.Time.time;
+        float dir = Mathf.Sin(t * .8f) + Mathf.Cos(t * .9f) / 2f;
+        float noise = Mathf.Cos(t * .8f) + Mathf.Sin(t * .9f) / 2f;
+        if (noise > 0f) Walk((Mathf.Pow(dir, 2f) * Mathf.Sign(dir)));
+        else Run((Mathf.Pow(dir, 2f) * Mathf.Sign(dir)));
+      }
+      // TODO: なんもない時うろうろしたい
+
+      if (m_Tween != null) m_Tween.Kill();
+      m_Tween = DOTween.To(
+          () => m_TargetDirection,
+          v => m_TargetDirection = v,
+          direction.x,
+          1f
+      );
     }
+  }
+
+  public void AddTarget(Fighter target)
+  {
+    m_Targets.Add(target);
+  }
+
+  void InvokeAttack()
+  {
+    float interval = 1f;
+    if (UnityEngine.Time.time - m_LastAttackedTime > interval)
+    {
+      Attack();
+      m_LastAttackedTime = UnityEngine.Time.time;
+    }
+  }
 
 }
--- StreetFighter/Assets/Scripts/Fighter/Fighter.cs
+++ StreetFighter/Assets/Scripts/Fighter/Fighter.cs
@@ -6,153 +6,157 @@ using UnityEngine.Events;
 public class Fighter : MonoBehaviour
 {
 
-    public string name = "undefined";
-    public float hp = 1;
-    public UnityEvent<Fighter> onDamaged = new UnityEvent<Fighter>();
+  public string name = "undefined";
+  public float hp = 1;
+  public UnityEvent<Fighter> onDamaged = new UnityEvent<Fighter>();
 
-    [SerializeField] protected GroundChecker m_GroundChecker;
-    [SerializeField] protected Attack m_Attack;
-    [SerializeField] protected Animator m_AnimCtrl;
-    [SerializeField] protected float m_Size = 2f;
-    [SerializeField] protected int m_AllowableJumpCnt = 2;
+  [SerializeField] Color m_Color;
+  [SerializeField] protected GroundChecker m_GroundChecker;
+  [SerializeField] protected Attack m_Attack;
+  [SerializeField] protected Animator m_AnimCtrl;
+  [SerializeField] protected float m_Size = 2f;
+  [SerializeField] protected int m_AllowableJumpCnt = 2;
 
-    protected Rigidbody2D m_RigidBody;
-    protected Vector2 m_Velocity = Vector2.zero;
-    protected Vector2 m_VelocityImpulse = Vector2.zero;
-    protected float m_Direction = 1f;
-    protected int m_JumpCnt = 0;
-    protected bool m_IsGround = false;
-    protected bool m_IsSitting = false;
-    protected bool m_IsFreezing = false;
+  protected Rigidbody2D m_RigidBody;
+  protected Vector2 m_Velocity = Vector2.zero;
+  protected Vector2 m_VelocityImpulse = Vector2.zero;
+  protected float m_Direction = 1f;
+  protected int m_JumpCnt = 0;
+  protected bool m_IsGround = false;
+  protected bool m_IsSitting = false;
+  protected bool m_IsFreezing = false;
 
-    bool m_IsFighting = false;  // 戦っているかどうかフラグ
+  bool m_IsFighting = false;  // 戦っているかどうかフラグ
 
-    protected void Awake()
+  protected void Awake()
+  {
+    m_RigidBody = GetComponent<Rigidbody2D>();
+    Cameraman.Instance.AddTarget(gameObject);
+    SpriteRenderer r = GetComponent<SpriteRenderer>();
+    r.material = new Material(r.material.shader);
+    r.material.SetColor("_Color", m_Color);
+  }
+
+  protected void Start()
+  {
+    transform.localScale = Vector3.one * m_Size;
+  }
+
+  protected void Update()
+  {
+
+  }
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    // もし敵の攻撃なら
+    // onDamaged.Invoke(this);  // 発火
+    if (collision.gameObject.tag == "Attack" && !collision.gameObject.Equals(m_Attack.gameObject))
     {
-        m_RigidBody = GetComponent<Rigidbody2D>();
-        Cameraman.Instance.AddTarget(gameObject);
+      Vector2 direction = transform.position - collision.gameObject.transform.position;
+      // m_VelocityImpulse += new Vector2(1f * Mathf.Sign(direction.x), .2f) * 10f;
+      m_RigidBody.AddForce(new Vector2(1f * Mathf.Sign(direction.x), .5f) * 5f, ForceMode2D.Impulse);
+      Cameraman.Instance.Shake();
+      m_IsFreezing = true;
+      CancelInvoke("UnFreeze");
+      Invoke("UnFreeze", .5f);
+      m_AnimCtrl.SetTrigger("Damaged");
     }
+  }
 
-    protected void Start()
+  /// <summary>
+  ///
+  /// </summary>
+  public void StartFighting()
+  {
+    m_IsFighting = true;
+    Debug.Log($"### {name} start fighting");
+  }
+
+  /// <summary>
+  ///
+  /// </summary>
+  public void EndFighting()
+  {
+    m_IsFighting = false;
+    Debug.Log($"### {name} end fighting");
+  }
+
+  public void Attack()
+  {
+    m_AnimCtrl.SetTrigger("Attack");
+    m_Attack.Execute(.1f, .3f);
+  }
+
+  public void Run(float input)
+  {
+    float vel = input * 100f;
+    Move(vel, 5f);
+  }
+
+  public void Walk(float input)
+  {
+    float vel = input * 100f;
+    Move(vel, 2f);
+  }
+
+  public void Jump()
+  {
+    if (m_GroundChecker.isGround) m_JumpCnt = 0;
+    if (m_JumpCnt < m_AllowableJumpCnt)
     {
-        transform.localScale = Vector3.one * m_Size;
+      // Jump
+      m_AnimCtrl.SetTrigger("Jump");
+      m_RigidBody.velocity = new Vector2(m_RigidBody.velocity.x, 0f);
+      m_RigidBody.AddForce(transform.up * 10f, ForceMode2D.Impulse);
+      m_JumpCnt++;
     }
+  }
 
-    protected void Update()
+  public void Sit(bool isDown)
+  {
+    m_IsSitting = isDown;
+    if (isDown)
     {
-
+      m_AnimCtrl.SetBool("Stand", false);
+      m_AnimCtrl.SetTrigger("Sit");
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    else
     {
-        // もし敵の攻撃なら
-        // onDamaged.Invoke(this);  // 発火
-        if(collision.gameObject.tag == "Attack" && !collision.gameObject.Equals(m_Attack.gameObject))
-        {
-            Vector2 direction = transform.position - collision.gameObject.transform.position;
-            // m_VelocityImpulse += new Vector2(1f * Mathf.Sign(direction.x), .2f) * 10f;
-            m_RigidBody.AddForce(new Vector2(1f * Mathf.Sign(direction.x), .5f) * 5f, ForceMode2D.Impulse);
-            Cameraman.Instance.Shake();
-            m_IsFreezing = true;
-            CancelInvoke("UnFreeze");
-            Invoke("UnFreeze", .5f);
-            m_AnimCtrl.SetTrigger("Damaged");
-        }
+      m_AnimCtrl.SetBool("Stand", true);
     }
+  }
 
-    /// <summary>
-    ///
-    /// </summary>
-    public void StartFighting()
+  void Move(float vel, float max)
+  {
+    float direction = Mathf.Sign(vel);
+    if (vel != 0f) m_Direction = direction;
+    transform.localScale = new Vector3(
+        m_Direction * m_Size,
+        transform.localScale.y,
+        transform.localScale.z
+    );
+    if (vel != 0)
     {
-        m_IsFighting = true;
-        Debug.Log($"### {name} start fighting");
+      if (vel > 0)
+      {
+        m_RigidBody.AddForce(new Vector3(1f, 0f, 0f) * m_RigidBody.mass * vel * UnityEngine.Time.deltaTime, ForceMode2D.Impulse);
+        if (m_RigidBody.velocity.x > max)
+          m_RigidBody.velocity = new Vector2(max, m_RigidBody.velocity.y);
+      }
+      else
+      {
+        m_RigidBody.AddForce(new Vector3(1f, 0f, 0f) * m_RigidBody.mass * vel * UnityEngine.Time.deltaTime, ForceMode2D.Impulse);
+        if (m_RigidBody.velocity.x < -max)
+          m_RigidBody.velocity = new Vector2(-max, m_RigidBody.velocity.y);
+      }
     }
+    m_AnimCtrl.SetFloat("Speed", Mathf.Abs(m_RigidBody.velocity.x));
+  }
 
-    /// <summary>
-    ///
-    /// </summary>
-    public void EndFighting()
-    {
-        m_IsFighting = false;
-        Debug.Log($"### {name} end fighting");
-    }
-
-    public void Attack()
-    {
-        m_AnimCtrl.SetTrigger("Attack");
-        m_Attack.Execute(.1f, .3f);
-    }
-
-    public void Run(float input)
-    {
-        float vel = input * 100f;
-        Move(vel, 5f);
-    }
-
-    public void Walk(float input)
-    {
-        float vel = input * 100f;
-        Move(vel, 2f);
-    }
-
-    public void Jump()
-    {
-        if(m_GroundChecker.isGround) m_JumpCnt = 0;
-        if(m_JumpCnt < m_AllowableJumpCnt)
-        {
-            // Jump
-            m_AnimCtrl.SetTrigger("Jump");
-            m_RigidBody.velocity = new Vector2(m_RigidBody.velocity.x, 0f);
-            m_RigidBody.AddForce(transform.up * 10f, ForceMode2D.Impulse);
-            m_JumpCnt++;
-        }
-    }
-
-    public void Sit(bool isDown)
-    {
-        m_IsSitting = isDown;
-        if(isDown)
-        {
-            m_AnimCtrl.SetBool("Stand", false);
-            m_AnimCtrl.SetTrigger("Sit");
-        }
-        else
-        {
-            m_AnimCtrl.SetBool("Stand", true);
-        }
-    }
-
-    void Move(float vel, float max)
-    {
-        float direction = Mathf.Sign(vel);
-        if(vel != 0f) m_Direction = direction;
-        transform.localScale = new Vector3(
-            m_Direction * m_Size,
-            transform.localScale.y,
-            transform.localScale.z
-        );
-        if(vel != 0)
-        {
-            if(vel > 0)
-            {
-                m_RigidBody.AddForce(new Vector3(1f, 0f, 0f) * m_RigidBody.mass * vel * UnityEngine.Time.deltaTime, ForceMode2D.Impulse);
-                if(m_RigidBody.velocity.x > max)
-                    m_RigidBody.velocity = new Vector2(max, m_RigidBody.velocity.y);
-            }
-            else
-            {
-                m_RigidBody.AddForce(new Vector3(1f, 0f, 0f) * m_RigidBody.mass * vel * UnityEngine.Time.deltaTime, ForceMode2D.Impulse);
-                if(m_RigidBody.velocity.x < -max)
-                    m_RigidBody.velocity = new Vector2(-max, m_RigidBody.velocity.y);
-            }
-        }
-        m_AnimCtrl.SetFloat("Speed", Mathf.Abs(m_RigidBody.velocity.x));
-    }
-
-    void UnFreeze()
-    {
-        m_IsFreezing = false;
-    }
+  void UnFreeze()
+  {
+    m_IsFreezing = false;
+  }
 
 }
